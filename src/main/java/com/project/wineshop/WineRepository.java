39bdfd925ebcc5cf24dca3b1ebda236ef0582ed7
Andrés Fernández <andresfera99@gmail.com>
2022-05-30 09:19:02 +0200

    Cambio query de mejoras años con avg

--- src/main/java/com/project/wineshop/WineRepository.java
+++ src/main/java/com/project/wineshop/WineRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 interface WineRepository extends JpaRepository<Wine, Long> {
     @Query(value = "SELECT * FROM wine ORDER BY rating DESC limit ?1", nativeQuery = true)
-    List<Wine> findBest(int num);
+    List<Wine> findBest(int num); //se puede usar Pageable para implementar un offset y se usa con PageRequest
 
     @Query(value = "SELECT * FROM wine ORDER BY price DESC limit ?1", nativeQuery = true)
     List<Wine> findExpensive(int num);
@@ -17,7 +17,7 @@ interface WineRepository extends JpaRepository<Wine, Long> {
     List<Wine> findBang(int num);
 
     @Query(value = "SELECT *\n" +
-            "FROM wine as w inner join (select `year` from wine group by `year` order by sum(rating) desc limit ?1) as w2\n" +
+            "FROM wine as w inner join (select `year` from wine group by `year` order by avg(rating) desc limit ?1) as w2\n" +
             "on w.`year` = w2.`year`\n" +
             "order by w.`year` DESC", nativeQuery = true)
     List<Wine> findVintage(int num);
